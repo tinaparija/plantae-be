eceed73d65556db91e8bbf163789322d7bf593d7
tinaparija <tinaparija@gmail.com>
2018-05-18 12:14:30 -0700

    env and schema updates

--- models/message.js
+++ models/message.js
@@ -2,11 +2,15 @@ var mongoose = require('mongoose');
     Schema = mongoose.Schema;
 
 var MessageSchema = new Schema({
-  email: String,
-  name: String,
-  ordered_plant: String, 
-  quantity: Number, 
-  message: String,
+    name: String, 
+    email: String, 
+    phone: String, 
+    zipcode: Number, 
+    monsteraQ: Number, 
+    pothosQ: Number, 
+    flfQ: Number, 
+    snakeQ: Number, 
+    comments: String 
 });
 
 var Message = mongoose.model('Message', MessageSchema);
