4fd2d230d03a4aca2036ade02e6917368ddda6b1
tinaparija <tinaparija@gmail.com>
2018-06-03 18:07:57 -0700

    email functionality

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,15 @@
 var models = require('../models');
 var Message = models.Message; 
+const nodemailer = require('nodemailer'); 
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+      user: 'plantae.sf@gmail.com',
+      pass: 'Plant4eva'
+    }
+});
+
 
 function index(req, res) {
   Message.find({}, function(err, messages) {
@@ -9,9 +19,17 @@ function index(req, res) {
 }
 
 function create(req, res) {
-   Message.create(req.body, function(err, message) {
+  Message.create(req.body, function(err, message) {
     if (err) { console.log('error', err); }
     res.json(message);
+
+    let mailOptions = {
+      from: 'plantae.sf@gmail.com', 
+      to: 'plantae.sf@gmail.com', 
+      subject: "A customer wants to get in touch!",
+      text: `${message}`
+    }
+    transporter.sendMail(mailOptions)
   });
 }
 
